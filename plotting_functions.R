116ab6749c04932aad369063fdc34e0aecc739c0
sophhan <79644349+sophhan@users.noreply.github.com>
2024-10-14 18:22:40 +0200

    update plotting_functions.R

--- plotting_functions.R
+++ plotting_functions.R
@@ -3,17 +3,24 @@
 
 
 ## Pfi plot --------------------------------------------------------------------
-plot_fi <- function(df_pfi,
-                    model = "coxph",
-                    color_values = c("#009E73", "#0072B2", "#CC79A7"),
-                    breaks = c(seq(0, 5, 1))) {
-    fi_plot <- ggplot(df_pfi,
-                      aes(
-                          x = time,
-                          y = values,
-                          color = features,
-                          linetype = features
-                      )) +
+#' Plot permutation feature importance
+#'
+#' @param df_pfi data.frame - contains pfi values, must contain a time, features and values column.
+#' @param model string - name of the machine learning model, by default "coxph".
+#' @param color_values vector - vector of colors used for plotting.
+#' @param breaks vector - to set ggplot x-axis breaks, by default "seq(0, 5, 1)".
+#' @returns A ggplot object.
+plot_pfi <- function(df_pfi,
+                     model = "coxph",
+                     color_values = c("#009E73", "#0072B2", "#CC79A7"),
+                     breaks = seq(0, 5, 1)) {
+    pfi_plot <- ggplot(df_pfi,
+                       aes(
+                           x = time,
+                           y = values,
+                           color = features,
+                           linetype = features
+                       )) +
         geom_line(linewidth = 0.9) +
         scale_color_manual(values = color_values) +
         scale_linetype_discrete() +
@@ -36,12 +43,25 @@ plot_fi <- function(df_pfi,
                 linewidth = 0.3
             )
         )
-    return(fi_plot)
+    return(pfi_plot)
 }
 
 
 
 ## Ice and pdp plots -----------------------------------------------------------
+#' Plot ice and pdp curves in one common plot
+#'
+#' @param df_ice data.frame - contains ice values, must contain a time, yhat and ids column and one column with the variable name.
+#' @param df_pdp data.frame - contains pdp values, must contain a time, yhat  and ids column and one column with the variable name.
+#' @param model string - name of the machine learning model, by default "coxph".
+#' @param variable unquoted feature argument -  column containing feature values.
+#' @param variable_name string - name of feature for which ice and pdp values should be computed, by default "treatment".
+#' @param time_var unquoted time argument - column containing the event times
+#' @param status_var unquoted status argument - column containing the status indicator
+#' @param limits vector - to set ggplot y-axis limits, by default "c(0, 1)".
+#' @param breaks_x vector - to set ggplot x-axis breaks, by default "seq(0, 1, by = 0.2)".
+#' @param breaks_y vector - to set ggplot y-axis breaks, by default "seq(0, 1, by = 0.2)".
+#' @returns A ggplot object.
 plot_ice_pdp <- function(df_ice,
                          df_pdp,
                          model = "coxph",
@@ -128,6 +148,14 @@ plot_ice_pdp <- function(df_ice,
 
 
 ## Ice plots -------------------------------------------------------------------
+#' Plot ice curves
+#'
+#' @param df_ice data.frame - contains ice values, must contain a time, yhat and ids column and one column with the variable name.
+#' @param model string - name of the machine learning model, by default "coxph".
+#' @param limits_y vector - to set ggplot y-axis limits, by default "c(0, 1)".
+#' @param breaks_y vector - to set ggplot y-axis breaks, by default "seq(0, 1, by = 0.2)".
+#' @param breaks_x vector - to set ggplot x-axis breaks, by default "seq(15, 50, 5)".
+#' @returns A ggplot object.
 plot_ice <- function(df_ice,
                      model = "coxph",
                      limits_y = c(0, 1),
@@ -192,6 +220,17 @@ plot_ice <- function(df_ice,
 
 
 ## Ale/pdp plots ---------------------------------------------------------------
+#' Plot ale or pdp curves
+#'
+#' @param df_ale_pdp data.frame - contains ale or pdp values, must contain a time, value and prediction column.
+#' @param model string - name of the machine learning model, by default "ranger".
+#' @param x_label string - name of the x-axis label, by default "x1".
+#' @param limits vector - to set ggplot y-axis limits, by default "c(0, 1)".
+#' @param breaks_x vector - to set ggplot x-axis breaks, by default "seq(0, 5, by = 1)".
+#' @param breaks_y vector - to set ggplot y-axis breaks, by default "seq(0, 1, by = 0.2)".
+#' @param key_width numeric - to control the width of the keys in the legend of a plot, by default 2.
+#' @param key_spacing numeric - to control the spacing between the keys in the legend, by default 0.5.
+#' @returns A ggplot object.
 plot_ale_pdp <- function(df_ale_pdp,
                          model = "ranger",
                          x_label = "x1",
@@ -247,6 +286,13 @@ plot_ale_pdp <- function(df_ale_pdp,
 
 
 ## individual shap plots -------------------------------------------------------
+#' Plot SurvSHAP(t) curves for individual observations
+#'
+#' @param df_shap data.frame - contains SurvSHAP(t) values, must contain a time, value and ind column.
+#' @param limits vector - to set ggplot y-axis limits, by default "c(-0.3, 0.1)".
+#' @param breaks vector - to set ggplot y-axis breaks, by default "seq(-0.3, 0.1, by = 0.1)".
+#' @param title string - plot title, by default "P1: Patient dead at t = 471".
+#' @returns A ggplot object.
 plot_shap_ind <- function(df_shap,
                           limits = c(-0.3, 0.1),
                           breaks = seq(-0.3, 0.1, by = 0.1),
@@ -312,6 +358,10 @@ plot_shap_ind <- function(df_shap,
 
 
 ## Aggregated shap line plot ---------------------------------------------------
+#' Plot aggregated SurvSHAP(t) over all observations
+#'
+#' @param df_shap data.frame - contains aggregated SurvSHAP(t) values, must contain a times, values and ind column.
+#' @returns A ggplot object.
 plot_shap_agg_line <- function(df_shap) {
     shap_plot <- ggplot() +
         geom_path(
@@ -370,6 +420,12 @@ plot_shap_agg_line <- function(df_shap) {
 
 
 ## Shap beeswarm plot for continuous feature -----------------------------------
+#' Plot beeswarm plots of SurvSHAP(t) values for continuous feature
+#'
+#' @param df_beeswarm data.frame - contains SurvSHAP(t) values of continuous feature, must contain a shap_value, value and feature column.
+#' @param x-lab string - x-lab title, by default "Aggregated SurvSHAP(t) value".
+#' @param subtitle string - plot subtitle, by default "ranger".
+#' @returns A ggplot object.
 plot_shap_bee_cont <- function(df_beeswarm,
                                xlab = "Aggregated SurvSHAP(t) value",
                                subtitle = "ranger") {
@@ -402,6 +458,12 @@ plot_shap_bee_cont <- function(df_beeswarm,
 
 
 ## shap beeswarm plot for categorical feature ----------------------------------
+#' Plot beeswarm plots of SurvSHAP(t) values for categorical feature
+#'
+#' @param df_beeswarm data.frame - contains SurvSHAP(t) values of categorical feature, must contain a shap_value, value and feature column.
+#' @param x-lab string - x-lab title, by default "Aggregated SurvSHAP(t) value".
+#' @param subtitle string - plot subtitle, by default "ranger".
+#' @returns A ggplot object.
 plot_shap_bee_cat <- function(df_beeswarm,
                               xlab = "Aggregated SurvSHAP(t) value",
                               subtitle = "ranger") {
@@ -435,6 +497,11 @@ plot_shap_bee_cat <- function(df_beeswarm,
 
 
 ## Survival function plot ------------------------------------------------------
+#' Plot survival curves
+#'
+#' @param df_sf data.frame - contains values to plot survival curves, must contain times, sfs and type column
+#' @param title string - plot title, by default "P1: Patient dead at t = 471".
+#' @returns A ggplot object.
 plot_sf <- function(df_sf, title = "P1: Patient dead at t = 471") {
     sf_plot <- ggplot(data = df_sf, aes(
         x = times,
@@ -478,9 +545,15 @@ plot_sf <- function(df_sf, title = "P1: Patient dead at t = 471") {
 
 
 ## Lime plot -------------------------------------------------------------------
+#' Plot SurvLIME importance values
+#'
+#' @param df_lime data.frame - contains values to plot SurvLIME importance values, must contain local_importance, variable_names, abs and sign_local_importance column
+#' @param title string - plot title, by default "P1: Patient dead at t = 471".
+#' @param accuracy numeric - to control the rounding precision of the labels.
+#' @returns A ggplot object.
 plot_lime <- function(df_lime,
                       title = "P1: Patient dead at t = 471",
-                      accuracy = accuracy) {
+                      accuracy = 0.0000001) {
     lime_plot <- ggplot(data = df_lime,
                         aes(
                             x = local_importance,
@@ -511,7 +584,16 @@ plot_lime <- function(df_lime,
 }
 
 # feature interaction plot
-
+#' Plot SurvLIME importance values
+#'
+#' @param df_Hjk data.frame - contains values to plot h-statistics values, must contain time, H and feature column
+#' @param title string - plot title, by default "x1".
+#' @param subtitle string - plot subtitle, by default "ranger".
+#' @param color_values vector - vector of colors used for plotting.
+#' @param limits_y vector - to set ggplot y-axis limits, by default "c(0, 1)".
+#' @param breaks_y vector - to set ggplot y-axis breaks, by default "seq(0, 1, by = 0.25)".
+#' @param breaks_x vector - to set ggplot x-axis breaks, by default "seq(15, 20, 5)".
+#' @returns A ggplot object.
 plot_f_inter <- function(df_Hjk,
                          title = "x1",
                          subtitle = "ranger",
